7a2505387bd7813d4498f5102241e927114cd7b1
Chris Jones <chris@axismedtech.com>
2016-04-26 12:43:04 -0700

    work

--- src/main/java/io/clickhandler/reactGwt/client/react/ExternalComponent.java
+++ src/main/java/io/clickhandler/reactGwt/client/react/ExternalComponent.java
@@ -1,49 +1,65 @@
 package io.clickhandler.reactGwt.client.react;
 
-import elemental.client.Browser;
+import io.clickhandler.gwtLumberjack.client.Logger;
 import io.clickhandler.reactGwt.client.Func;
 import io.clickhandler.reactGwt.client.Jso;
 import io.clickhandler.reactGwt.client.Reflection;
 import io.clickhandler.reactGwt.client.dom.DOM;
-import jsinterop.annotations.JsIgnore;
-
-import java.util.logging.Logger;
 
 
 public abstract class ExternalComponent<P> {
+
+    protected final Logger log = Logger.get(getClass());
 
     public ExternalComponent() {
     }
 
     protected abstract ReactClass<P> getReactClass();
 
-    private P keyedProps(P props) {
-        if (props == null) {
-            props = Jso.create();
-        }
-//        if (Reflection.get(props, "key") == null) {
-//            Reflection.set(props, "key", ChildCounter.get().newKey());
-//        }
+    /*
+     * Props
+     */
+
+    private P getProps() {
+        P props = Reflection.get(getReactClass(), "getDefaultProps") != null ? getReactClass().getDefaultProps() : Jso.create();
+        // todo apply key?
         return props;
     }
+
+    /*protected void applyKey(P props) {
+        log.trace("applyKey(props)", props);
+        Object key = Reflection.get(props, "key");
+        if (key == null) {
+            Reflection.set(props, "key", io.clickhandler.reactGwt.client.react.ChildCounter.get().newKey());
+        }
+    }*/
 
     /*
      * Factory Methods
      */
+
     public ReactElement createElement() {
-        return React.createElement(getReactClass(), keyedProps(getReactClass().getDefaultProps()));
+        log.trace("createElement()");
+        return React.createElement(getReactClass(), getProps());
     }
 
     public ReactElement createElement(Object... children) {
-        return React.createElement(getReactClass(), keyedProps(getReactClass().getDefaultProps()), children);
+        log.trace("createElement(children)", children);
+        return React.createElement(getReactClass(), getProps(), children);
     }
 
     public ReactElement createElement(P props) {
-        return React.createElement(getReactClass(), keyedProps(props));
+        log.trace("createElement(props)", props);
+        if (props == null) {
+            props = Jso.create();
+        }
+        Reflection.assign(props, getProps());
+        return React.createElement(getReactClass(), props);
     }
 
     public ReactElement createElement(Func.Run1<P> propsCallback) {
-        final P props = keyedProps(getReactClass().getDefaultProps());
+        log.trace("createElement(propsCallback)", propsCallback);
+        final P props = getProps();
         if (propsCallback != null) {
             propsCallback.run(props);
         }
@@ -51,7 +67,8 @@ public abstract class ExternalComponent<P> {
     }
 
     public ReactElement createElement(Func.Run1<P> propsCallback, Object... children) {
-        final P props = keyedProps(getReactClass().getDefaultProps());
+        log.trace("createElement(propsCallback, children)", propsCallback, children);
+        final P props = getProps();
         if (propsCallback != null) {
             propsCallback.run(props);
         }
@@ -59,7 +76,8 @@ public abstract class ExternalComponent<P> {
     }
 
     public ReactElement createElement(Func.Run2<P, DOM.ChildList> callback) {
-        final P props = keyedProps(getReactClass().getDefaultProps());
+        log.trace("createElement(Run2<props, children>)", callback);
+        final P props = getProps();
         final DOM.ChildList childList = new DOM.ChildList();
         if (callback != null) {
             callback.run(props, childList);
@@ -91,31 +109,5 @@ public abstract class ExternalComponent<P> {
 
     public ReactElement $(Func.Run2<P, DOM.ChildList> callback) {
         return createElement(callback);
-    }
-
-    /*
-     * Remove this stuff?
-     */
-    // needed ?
-    protected void initProps(P props) {
-    }
-
-    protected P defaultProps() {
-        final P props = Jso.create();
-        try {
-            initProps(props);
-            Reflection.assign(props, getReactClass().getDefaultProps());
-            applyKey(props); // todo needed?
-        } catch (Throwable e) {
-            Browser.getWindow().getConsole().log(e);
-        }
-        return props;
-    }
-
-    protected void applyKey(P props) {
-        Object key = Reflection.get(props, "key");
-        if (key == null) {
-            Reflection.set(props, "key", io.clickhandler.reactGwt.client.react.ChildCounter.get().newKey());
-        }
     }
 }
--- src/main/java/io/clickhandler/reactGwt/client/react/ReactClass.java
+++ src/main/java/io/clickhandler/reactGwt/client/react/ReactClass.java
@@ -1,8 +1,10 @@
 package io.clickhandler.reactGwt.client.react;
 
+import jsinterop.annotations.JsMethod;
 import jsinterop.annotations.JsType;
 
 @JsType(isNative = true)
 public interface ReactClass<P> {
+    @JsMethod
     P getDefaultProps();
 }
